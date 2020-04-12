56b42e5105e77e043a81399f9e2d49bd6d03dc07
unknown <o.adewale17@gmail.com>
2020-04-12 12:50:04 +0100

    arrange code

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,9 @@
 const covid19ImpactEstimator = (data) => {
-const input = data
 return {
-    data: input,
-    impact: {},
-    severeImpact:{}
-}
+         data: {},
+         impact: {},
+         severeImpact: {}
+       }
 }
 
 export default covid19ImpactEstimator;
