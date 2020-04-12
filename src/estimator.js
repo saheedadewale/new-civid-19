3ddb82ed8758a2d18001944a9d1568d0b3346bda
unknown <o.adewale17@gmail.com>
2020-04-12 01:08:06 +0100

    start the estimator building with

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+const data ={
+    region:{
+        name: "africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+}
+return {
+    data:{},
+    impact: {},
+    severeImpact:{}
+
+}
+}
 
 export default covid19ImpactEstimator;
