69b971c2a77bbe72c4c134ed393c804e43b0a609
unknown <o.adewale17@gmail.com>
2020-04-12 12:22:05 +0100

    variable

--- src/estimator.js
+++ src/estimator.js
@@ -1,21 +1,22 @@
 const covid19ImpactEstimator = (data) => {
 //input to the estimator function
-    const input = {
-    region:{
-        name: "africa",
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 5,
-        avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-}
+const input = data;
+//      data = {
+//     region:{
+//         name: "africa",
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// }
 //output from the estimator function
 return {
-    data:{},
+    data: input,
     impact: {},
     severeImpact:{}
 
