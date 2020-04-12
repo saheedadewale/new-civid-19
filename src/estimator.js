a0fae3551a50f9379791eb4645dd1cf9d2927f14
unknown <o.adewale17@gmail.com>
2020-04-12 15:07:24 +0100

    new mod

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,10 @@
 const covid19ImpactEstimator = (data) => {
+let input = data;
 return {
-         data: {},
+         data: data,
          impact: {},
          severeImpact: {}
        }
-}
+};
 
 export default covid19ImpactEstimator;
