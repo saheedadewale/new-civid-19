a89e89ee5f0165771d91e8a87d102686a3ee909e
unknown <o.adewale17@gmail.com>
2020-04-13 04:28:23 +0100

    troubleshooting

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,24 @@
 const covid19ImpactEstimator = (data) => {
-// Determin factors
-    let myFactor;
-      if (data.periodType === 'days'){
-      myFactor = Math.trunc(data.timeToElapse / 3);   
-    }
-    else if (data.periodType === 'weeks'){
-      myFactor = Math.trunc((data.timeToElapse * 7)/3);
-    }
-    else if (data.periodType === 'months')
-    {
-      myFactor = Math.trunc((data.timeToElapse * 30)/3);
-    }
-    let impact = {};
-    let severeImpact = {};
-    impact.currentlyInfected = data.reportedCases * 10;
-    severeImpact.currentlyInfected = data.reportedCases * 50;
-    impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, myFactor);
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, myFactor);
+  let myFactor;
+  if (data.periodType === 'days') {
+    myFactor = Math.trunc(data.timeToElapse / 3);
+  } else if (data.periodType === 'weeks') {
+    myFactor = Math.trunc((data.timeToElapse * 7) / 3);
+  } else if (data.periodType === 'months') {
+    myFactor = Math.trunc((data.timeToElapse * 30) / 3);
+  }
+
+  const impact = {};
+  const severeImpact = {}
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, myFactor);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, myFactor);
   return {
-    data: data,
-    impact: impact,
-    severeImpact: severeImpact
+    data,
+    impact,
+    severeImpact
   };
 };
 
 export default covid19ImpactEstimator;
-
