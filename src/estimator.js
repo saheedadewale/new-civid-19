6b0d7b973c19dbbf7e38e6d3b1cf8320f72b7696
unknown <o.adewale17@gmail.com>
2020-04-14 20:24:00 +0100

    update formula

--- src/estimator.js
+++ src/estimator.js
@@ -28,7 +28,6 @@ const covid19ImpactEstimator = (data) => {
   const severeV = (0.02 * severeImpact.infectionsByRequestedTime);
   impact.casesForVentilatorsByRequestedTime = Math.trunc(impactV);
   severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeV);
-  
   const impactD = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
   const severeD = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
   const iInc = impactD * data.region.avgDailyIncomeInUSD;
