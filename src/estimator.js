485412ba38fbcd2865b75289af48a7f35204425a
unknown <o.adewale17@gmail.com>
2020-04-14 17:17:26 +0100

    debuging

--- src/estimator.js
+++ src/estimator.js
@@ -28,12 +28,6 @@ const covid19ImpactEstimator = (data) => {
   const severeV = (2 / 100) * severeImpact.infectionsByRequestedTime;
   impact.casesForVentilatorsByRequestedTime = Math.trunc(impactV);
   severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeV);
-  const impactD = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
-  const severeD = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
-  const iInc = data.region.avgDailyIncomeInUSD * data.timeToElapse;
-  const sInc = data.region.avgDailyIncomeInUSD * data.timeToElapse;
-  impact.dollarsInFlight = Math.trunc(impactD * iInc);
-  severeImpact.dollarsInFlight = Math.trunc(severeD * sInc);
   return {
     data,
     impact,
